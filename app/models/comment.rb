bc5dcedf0c32fa8896ae3b5ac433870824768397
Ryan Lewin <lewin.ry+development@gmail.com>
2020-07-07 08:12:26 +1000

    Adds Comment model and associates with User and Post - Challenge complete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  belongs_to :post
 
   validates :content, presence: true
   validates :user_id, presence: true
+  validates :post_id, presence: true
 end
